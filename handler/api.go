dc1d838845fb45693f39ef9f580194b203eb331a
Apple <aldi.madridista.am@gmail.com>
2024-12-23 20:21:10 +0700

    feat(github): test updated

--- handler/api.go
+++ handler/api.go
@@ -8,6 +8,6 @@ import (
 
 // Hello handle api status
 func Hello(c *fiber.Ctx) error {
-    fmt.Println("Hello endpoint reached") // Debug log
-    return c.SendString("Hello, World!")
+	fmt.Println("Hello endpoint reached") // Debug log
+	return c.SendString("Hello, World updated!")
 }
