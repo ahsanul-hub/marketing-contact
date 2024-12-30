2261788f2aa0f2894b8343ca2da79249cbabdb94
Apple <aldi.madridista.am@gmail.com>
2024-12-30 11:08:18 +0700

    feat(api): update api hello

--- handler/api.go
+++ handler/api.go
@@ -9,5 +9,5 @@ import (
 // Hello handle api status
 func Hello(c *fiber.Ctx) error {
 	fmt.Println("Hello endpoint reached") // Debug log
-	return c.SendString("Hello, World updated!")
+	return c.SendString("Hello, from Redpay API!")
 }
