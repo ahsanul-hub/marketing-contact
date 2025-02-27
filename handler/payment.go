ab22b5b3cca34b3146e882b568efe869a1bf3172
Apple <aldi.madridista.am@gmail.com>
2025-02-27 15:08:54 +0700

    feat(payment): fix deleted function

--- handler/payment.go
+++ handler/payment.go
@@ -1,18 +1,14 @@
 package handler
 
 import (
-	"app/dto/http"
 	"app/dto/model"
 	"app/helper"
 	"app/lib"
 	"app/pkg/response"
 	"app/repository"
-	"context"
 	"math"
-	"strings"
 	"time"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/patrickmn/go-cache"
@@ -20,83 +16,6 @@ import (
 )
 
 var TransactionCache = cache.New(5*time.Minute, 6*time.Minute)
-
-func CreatePayment(c *fiber.Ctx) error {
-	headers := map[string]string{
-		"appkey":    c.Get("appkey"),
-		"appid":     c.Get("appid"),
-		"timestamp": c.Get("timestamp"),
-		"nonce":     c.Get("nonce"),
-		"secret":    c.Get("secret"),
-		"bodysign":  c.Get("bodysign"),
-	}
-
-	arrClient, err := repository.FindClient(context.Background(), c.Get("appkey"), c.Get("appid"))
-
-	if err != nil {
-		return response.Response(c, fiber.StatusBadRequest, "E0001")
-	}
-
-	// log.Println(arrClient)
-
-	var req http.CreatePaymentRequest
-
-	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid request body",
-		})
-	}
-
-	if err := validator.New().Struct(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": err.Error(),
-		})
-	}
-
-	// checksecret() cek code legacy
-
-	arrayTransactionCheck := CheckedTransaction(&req, arrClient)
-	if !arrayTransactionCheck["success"].(bool) {
-		return response.Response(c, fiber.StatusBadRequest, arrayTransactionCheck["retcode"].(string))
-	}
-
-	inputReq := model.InputPaymentRequest{
-		ClientAppKey: headers["appkey"],
-		Status:       helper.GetStatusMessage("1001"),
-		Mobile:       arrayTransactionCheck["mobile"].(string),
-		// Testing:       arrayTransactionCheck["testing"].(bool),
-		Route:         arrayTransactionCheck["route"].(string),
-		PaymentMethod: arrayTransactionCheck["payment_method"].(string),
-		ItemName:      req.ItemName,
-		Currency:      "IDR",
-		Price:         arrayTransactionCheck["charging_price"].(uint),
-	}
-
-	// Beautify UserMDN
-	if req.UserMDN != "" {
-		req.UserMDN = helper.BeautifyIDNumber(strings.TrimSpace(inputReq.UserMDN), false)
-	}
-
-	// Create the transaction order
-	transactionToken, err := repository.CreateOrder(context.Background(), &inputReq, arrClient)
-	if err != nil {
-		return response.Response(c, fiber.StatusInternalServerError, "E4001: Database Error")
-	}
-
-	// Save timestamps for transaction
-
-	// Need check timestamp, check code legacy
-	// err = SaveTransactionTimestamp(transactionToken)
-	// if err != nil {
-	// 	return response.Response(c, fiber.StatusInternalServerError, "E4001: Failed to update transaction timestamps")
-	// }
-
-	// Return successful response
-
-	return response.ResponseSuccess(c, fiber.StatusOK, fiber.Map{
-		"token": transactionToken,
-	})
-}
 
 func TestPayment(c *fiber.Ctx) error {
 	// Mendapatkan data dari request body
