116feaad712b9737ee8f050152b2df063c08ddfe
ahsanul-hub <aldi.madridista.am@gmail.com>
2026-02-05 10:39:26 +0700

    feat(next): update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,31 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: "",
       },
     ],
   },
-  // Disable performance measurement untuk menghindari error
-  experimental: {
-  },
+
   turbopack: {
-    // Paksa Turbopack menganggap root workspace ada di folder proyek ini
     root: process.cwd(),
   },
 };
 
 export default nextConfig;
-
